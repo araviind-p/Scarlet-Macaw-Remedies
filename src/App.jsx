8f8d200cc141ac69e7f7a8ba131f062b1bf4d4cc
Aravind P <araviindp@gmail.com>
2024-11-19 10:45:11 +0530

    Card change to grid

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,61 @@ const App = () => {
   return (
     <div>
       <Sidebar></Sidebar>
-      <div className='flex justify-start items-center'>
+      <div className='grid justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        <Card
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0IG5G4T7uNoezBW4L91PgCcrwvPIsytyCew&s"
+          title="Card Title"
+          description="This is a brief description of the card content. It provides a summary or context."
+          buttonText="Learn More"
+        />
+        <Card
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0IG5G4T7uNoezBW4L91PgCcrwvPIsytyCew&s"
+          title="Card Title"
+          description="This is a brief description of the card content. It provides a summary or context."
+          buttonText="Learn More"
+        />
+        <Card
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0IG5G4T7uNoezBW4L91PgCcrwvPIsytyCew&s"
+          title="Card Title"
+          description="This is a brief description of the card content. It provides a summary or context."
+          buttonText="Learn More"
+        />
+        <Card
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0IG5G4T7uNoezBW4L91PgCcrwvPIsytyCew&s"
+          title="Card Title"
+          description="This is a brief description of the card content. It provides a summary or context."
+          buttonText="Learn More"
+        />
+        <Card
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0IG5G4T7uNoezBW4L91PgCcrwvPIsytyCew&s"
+          title="Card Title"
+          description="This is a brief description of the card content. It provides a summary or context."
+          buttonText="Learn More"
+        />
+        <Card
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0IG5G4T7uNoezBW4L91PgCcrwvPIsytyCew&s"
+          title="Card Title"
+          description="This is a brief description of the card content. It provides a summary or context."
+          buttonText="Learn More"
+        />
+        <Card
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0IG5G4T7uNoezBW4L91PgCcrwvPIsytyCew&s"
+          title="Card Title"
+          description="This is a brief description of the card content. It provides a summary or context."
+          buttonText="Learn More"
+        />
+        <Card
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0IG5G4T7uNoezBW4L91PgCcrwvPIsytyCew&s"
+          title="Card Title"
+          description="This is a brief description of the card content. It provides a summary or context."
+          buttonText="Learn More"
+        />
+        <Card
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0IG5G4T7uNoezBW4L91PgCcrwvPIsytyCew&s"
+          title="Card Title"
+          description="This is a brief description of the card content. It provides a summary or context."
+          buttonText="Learn More"
+        />
         <Card
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0IG5G4T7uNoezBW4L91PgCcrwvPIsytyCew&s"
           title="Card Title"
