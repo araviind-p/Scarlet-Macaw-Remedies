1ea4783a2a72fa2b00e15713c991d4c171f855f4
Anand P <anandpamegoze@gmail.com>
2024-12-02 17:58:33 +0530

    change images

--- src/compnent/Navbar.jsx
+++ src/compnent/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { scroller } from 'react-scroll';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
-import Logo from '/logo.png';
+import Logo from '/navlogo.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
